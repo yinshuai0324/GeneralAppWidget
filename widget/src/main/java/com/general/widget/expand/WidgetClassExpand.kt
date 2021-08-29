480eb8820ebaa08673e9923d0321c29f23fcc4fe
_叮当 <1317972280@qq.com>
2021-08-29 22:30:40 +0800

    1.修复按钮点击事件不触发的Bug

--- widget/src/main/java/com/general/widget/expand/WidgetClassExpand.kt
+++ widget/src/main/java/com/general/widget/expand/WidgetClassExpand.kt
@@ -4,17 +4,6 @@ import android.content.res.Resources
 import android.util.TypedValue
 
 
-/**
- * Int转DP
- */
-val Int.dp: Int
-    get() {
-        return TypedValue.applyDimension(
-            TypedValue.COMPLEX_UNIT_DIP,
-            this.toFloat(),
-            Resources.getSystem().displayMetrics
-        ).toInt()
-    }
 
 /**
  * Float转DP
